35f1f3cf79546ead9abaabf890aeb3cae50d95ae
Michał Miotła <miotlamichal@gmail.com>
2023-11-04 13:02:37 +0100

    working with mobile view in main part

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 const burgerBtn = document.querySelector('.burger-btn')
-const burgerImg = document.querySelector('.burger-img')
+const burgerImgOpen = document.querySelector('.burger-img-open')
+const burgerImgCLose = document.querySelector('.burger-img-close')
 const mobileNav = document.querySelector('.mobile-nav')
 const navLinks = document.querySelectorAll('.link')
 
 const toggleNavigation = () => {
 	mobileNav.classList.toggle('translate-x-[100%]')
+	burgerImgOpen.classList.toggle('hidden')
+	burgerImgCLose.classList.toggle('hidden')
 }
 
 const closeNav = () => {
 	navLinks.forEach(link => {
 		link.addEventListener('click', () => {
 			mobileNav.classList.toggle('translate-x-[100%]')
+			if (mobileNav.classList.contains('translate-x-[100%]')) {
+				burgerImgCLose.classList.add('hidden')
+				burgerImgOpen.classList.remove('hidden')
+			}
 		})
 	})
 }
